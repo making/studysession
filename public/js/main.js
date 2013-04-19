362ddbb74886fb469cd0f126841cfa939e801453
Toshiaki Maki <makingx@gmail.com>
2013-04-19 17:06:20 +0900

    supported CRUD

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,3 @@
-/**
- * Created with IntelliJ IDEA.
- * User: maki
- * Date: 2013/04/15
- * Time: 0:25
- * To change this template use File | Settings | File Templates.
- */
 var studysession = {};
 
 // Models
@@ -33,9 +26,18 @@ studysession.CandidateListView = Backbone.View.extend({
     tagName: 'table',
     initialize: function () {
         this.collection.bind('reset', this.render, this);
+	var that = this;
+	this.collection.bind('add', function(candidate) {
+	    if (that.headerAppended) {
+		that.$el.append(new studysession.CandidateListItemView({
+		    model: candidate
+		}).render().el);
+	    }
+	});
     },
     render: function (eventName) {
 	this.$el.append('<tr><th>TITLE</th><th>GENRE</th><th>CREATOR</th><th>SPEAKER</th><th>LIKE</th></tr>');
+	this.headerAppended = true;
         this.collection.each(function (candidate) {
             this.$el.append(new studysession.CandidateListItemView({
                 model: candidate
@@ -48,17 +50,64 @@ studysession.CandidateListView = Backbone.View.extend({
 studysession.CandidateListItemView = Backbone.View.extend({
     tagName: 'tr',
     template: _.template($('#tpl-candidate-list-item').html()),
+    initialize: function () {
+	this.model.bind('change', this.render, this);
+	this.model.bind('destroy', this.close, this);
+    },
     render: function (eventName) {
         this.$el.html(this.template(this.model.toJSON()));
         return this;
+    },
+    close: function () {
+	this.$el.unbind();
+	this.$el.remove();
     }
 });
 
-studysession.CandidateView = Backbone.View.extend({
+studysession.CandidateFormView = Backbone.View.extend({
     template: _.template($('#tpl-candidate-form').html()),
+    initialize: function() {
+	this.model.bind('change', this.render, this);
+    },
     render: function (eventName) {
         this.$el.html(this.template(this.model.toJSON()));
         return this;
+    },
+    events: {
+	'change input' : 'change',
+	'click .save' : 'saveCandidate',
+	'click .delete' : 'deleteCandidate'
+    },
+    change: function (event) {
+	//var target = event.target;
+	//console.log('changing ' + target.id + ' from: ' + target.defaultValue + ' to: ' + target.value);	
+    },
+    saveCandidate: function() {
+	this.model.set({
+	    title: $('#title').val(),
+	    genre: $('#genre').val(),
+	    creator: $('#creator').val(),
+	    speaker: $('#speaker').val().split(',')
+	});
+	if (this.model.isNew()) {
+	    app.candidates.create(this.model);
+	} else {
+	    this.model.save();
+	}
+	return false;
+    },
+    deleteCandidate: function() {
+	this.model.destroy({
+	    success: function() {
+		alert('deleted successfully!');
+		window.history.back();
+	    }
+	});
+	return false;
+    },
+    close: function() {
+	this.$el.unbind();
+	this.$el.empty();
     }
 });
 
@@ -66,21 +115,42 @@ studysession.CandidateView = Backbone.View.extend({
 studysession.AppRouter = Backbone.Router.extend({
     routes: {
         '': 'list',
-        'candidates/:id': 'details'
+        'candidates/:id': 'details',
+	'form': 'form'
+    },
+    initialize: function() {
     },
     list: function () {
         this.candidates = new studysession.Candidates();
-        this.candidateListView = new studysession.CandidateListView({collection: this.candidates});
+        this.candidateListView = new studysession.CandidateListView({
+	    collection: this.candidates
+	});
 	var that = this;
-        this.candidates.fetch({success: function() {
-	    $('#candidate-form').html('');
-	    $('#candidate-list').html(that.candidateListView.render().el);
-	}});
+        this.candidates.fetch({
+	    success: function() {
+		$('#candidate-form').html('');
+		$('#candidate-list').html(that.candidateListView.render().el);
+	    }
+	});
     },
     details: function (id) {
         this.candidate = this.candidates.get(id);
-        this.candidateView = new studysession.CandidateView({model: this.candidate});
+	if (this.candidateView) {
+	    this.candidateView.close();
+	}
+        this.candidateView = new studysession.CandidateFormView({
+	    model: this.candidate
+	});
         $('#candidate-form').html(this.candidateView.render().el);
+    },
+    form: function() {
+	if (this.candidateView) {
+	    this.candidateView.close();
+	}
+	this.candidateView = new studysession.CandidateFormView({
+	    model: new studysession.Candidate()
+	});
+	$('#candidate-form').html(this.candidateView.render().el);
     }
 });
 
